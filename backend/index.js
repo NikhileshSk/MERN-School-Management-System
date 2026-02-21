e20eff0d8401f1497b592305a953d71bb0d9a500
nikhilesh-SK <nikhileshsk369@gmail.com>
2026-02-21 22:56:47 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,7 @@ mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
+
     })
     .then(console.log("Connected to MongoDB"))
     .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
